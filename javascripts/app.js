d04216f73a461d5ff6d6bcd66df4aac687fc7c61
mikezalik <mike.zalik@gmail.com>
2019-02-16 09:50:58 -0800

    user route

--- javascripts/app.js
+++ javascripts/app.js
@@ -13,3 +13,14 @@ function homeRoute (request, response) {
     response.end('footer\n');
     }
 }
+
+function userRoute(request, response) {
+    var username = request.url.replace("/", "");
+    if(username.length > 0) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write("Header\n");
+        response.write("username\n");
+        response.end('footer\n');
+
+    }
+}
