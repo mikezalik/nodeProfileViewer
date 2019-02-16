8497e1424f2259184ac4faa81437924d66df2710
mikezalik <mike.zalik@gmail.com>
2019-02-16 09:39:42 -0800

    home route

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,13 @@
 var http = require('http');
 http.createServer(function (request,response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    setInterval(function(){
-        response.write(new Date() + "\n");
-    }, 1000);
+   homeRoute(request, response);
 }).listen(3000);
 console.log('Server running at http://<workspace-url>/');
+
+
+function homeRoute (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Header\n");
+    response.write("Search\n");
+    response.end('footer\n');
+}
