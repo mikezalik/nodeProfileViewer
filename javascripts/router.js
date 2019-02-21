e001b7798803f4f34b4d1fe07def1dd51667fc24
mikezalik <mike.zalik@gmail.com>
2019-02-20 17:37:29 -0800

    router refactor

--- javascripts/router.js
+++ javascripts/router.js
@@ -4,60 +4,86 @@ var querystring = require("querystring");
 
 var commonHeaders = {'Content-Type': 'text/html'};
 
-//handle HTTP route GET / and POST /
+//Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
-    if(request.url === "/") {
-        if(request.method.toLowerCase() === "get") {
-    response.writeHead(200, commonHeaders);
-    renderer.view("header", {}, response);
-    renderer.view("search", {}, response);
-    renderer.view("footer", {}, response);
-    response.write("Search\n");
-    response.end();
+  //if url == "/" && GET
+  if(request.url === "/") {
+    if(request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, commonHeaders);  
+      renderer.view("header", {}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
     } else {
-        //get POST data from body
-    request.on("data", function(postBody) {
+      //if url == "/" && POST
+      
+      //get the post data from body
+      request.on("data", function(postBody) {
+        //extract the username
         var query = querystring.parse(postBody.toString());
-        response.writeHead(303, {"Location": "/" + query.username});
+        response.write(query.username);
         response.end();
-            });
-        }
+        //redirect to /:username
+      });
+      
     }
+  }
+  
 }
 
-//handle HTTP route GET /:username
+//Handle HTTP route GET /:username i.e. /chalkers
 function user(request, response) {
-    var username = request.url.replace("/", "");
-    if(username.length > 0) {
-        response.writeHead(200, commonHeaders);
-        renderer.view("header", {}, response);
-        //get JSON
-        var studentProfile = new Profile(username);
+  //if url == "/...."
+  var username = request.url.replace("/", "");
+  if(username.length > 0) {
+    response.writeHead(200, commonHeaders);  
+    renderer.view("header", {}, response);    
+    
+    //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on "end"
+    studentProfile.on("end", function(profileJSON){
+      //show profile
+      
+      //Store the values which we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url, 
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      //Simple response
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    });
         
-        studentProfile.on("end", function(profileJSON){
-            
-            var values = {
-                avatarURL: profileJSON.gravatar_url,
-                username: profileJSON.profile_name,
-                badges: profileJSON.badges.length,
-                javascriptPoints: profileJSON.points.JavaScript,
-            }
-            //simple response
-            renderer.view("profile", values, response);
-            renderer.view("footer", values, response);
-            response.end();
-        });
-        studentProfile.on("error", function(error){
-            renderer.view("error", {errorMessage: error.message}, response);
-            renderer.view("search", {}, response);
-            renderer.view("footer", {}, response);
-            response.end();
-        });
-
-    }
+    //on "error"
+    studentProfile.on("error", function(error){
+      //show error
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    });
+      
+  }
 }
-
 
 module.exports.home = home;
 module.exports.user = user;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
